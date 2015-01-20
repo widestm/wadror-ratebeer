2d431d1832b7499cd9f04020485390f009f4d8bd
Mikael Wide <mikael.wide@helsinki.fi>
2015-01-21 00:25:14 +0200

    Better forms for adding beers

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -15,11 +15,15 @@ class BeersController < ApplicationController
   # GET /beers/new
   def new
     @beer = Beer.new
+    @breweries = Brewery.all
+    @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
   end
 
   # GET /beers/1/edit
   def edit
-  end
+   @breweries = Brewery.all
+   @styles = ["Weizen", "Lager", "Pale ale", "IPA", "Porter"]
+ end
 
   # POST /beers
   # POST /beers.json
@@ -28,7 +32,7 @@ class BeersController < ApplicationController
 
     respond_to do |format|
       if @beer.save
-        format.html { redirect_to @beer, notice: 'Beer was successfully created.' }
+        format.html { redirect_to beers_path, notice: 'Beer was successfully created.' }
         format.json { render :show, status: :created, location: @beer }
       else
         format.html { render :new }
@@ -71,4 +75,4 @@ class BeersController < ApplicationController
     def beer_params
       params.require(:beer).permit(:name, :style, :brewery_id)
     end
-end
+  end
