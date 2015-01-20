6c0e6d2d1af7bf51adf03d5fc3e44346b09ed3f7
Mikael Wide <mikael.wide@helsinki.fi>
2015-01-20 22:57:21 +0200

    Added ratings to app

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,16 @@
 class Beer < ActiveRecord::Base
 	belongs_to :brewery
+	has_many :ratings
+
+	def average_rating
+		# total_score = 0.0
+		# self.ratings.each do |rat|
+		# 	total_score += rat.score
+		# end
+		# avg_rating = 0.0
+		# avg_rating = total_score/self.ratings.count
+		# avg_rating.round(2)
+		
+		self.ratings.average(:score).round(2)
+	end
 end
