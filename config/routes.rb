6c6e8bae19c7a2dc516bdd941b40e655aa953ebd
Mikael Wide <mikael.wide@helsinki.fi>
2015-01-26 19:49:06 +0200

    Views edited to handle users

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,12 @@ Rails.application.routes.draw do
 
   resources :ratings, only: [:index, :new, :create, :destroy]
 
-  get 'signup', to: 'users#new'
 
   resource :session, only: [:new, :create, :delete]
+
+  get 'signup', to: 'users#new'
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
 
   root 'breweries#index'
 
