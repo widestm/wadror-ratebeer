de3e2d9925bcd36d5ea2257514962a979937ca71
Mikael Wide <mikael.wide@helsinki.fi>
2015-01-26 19:54:33 +0200

    Added relations between user and ratings

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,7 @@
 class Rating < ActiveRecord::Base
 	belongs_to :beer
-
+	belongs_to :user
+	
 	def to_s
 		"#{self.beer.name} #{self.score}"
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150126165609) do
+ActiveRecord::Schema.define(version: 20150126175101) do
 
   create_table "beers", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150126165609) do
     t.integer  "beer_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
