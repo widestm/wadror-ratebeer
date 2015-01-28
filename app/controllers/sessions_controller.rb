fd0621cc27ecf8875920952a7e61f945a81dc32b
Mikael Wide <mikael.wide@helsinki.fi>
2015-01-28 15:49:26 +0200

    Signin has notices and redirects back if user not found

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,13 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by username: params[:username]
-
-		session[:user_id] = user.id unless user.nil?
-
-		redirect_to user
+		if user.nil?
+			redirect_to :back, notice: "User #{params[:username]} does not exist!"
+		else
+			session[:user_id] = user.id unless user.nil?
+			redirect_to user, notice: "Welcome back #{params[:username]}!"
+		end
 	end
-
 	def destroy
 		session[:user_id] = nil
 
