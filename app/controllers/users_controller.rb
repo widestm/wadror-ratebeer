cbda4949388f3679fdf0263bfb4402a5827ecbad
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-01 14:35:19 +0200

    Better redirections when handling users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to users_path, notice: 'User was successfully created.' }
+        format.html { redirect_to new_session_path, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
@@ -59,7 +59,7 @@ class UsersController < ApplicationController
      session[:user_id] = nil 
      @user.destroy
      respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to breweries_path, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
