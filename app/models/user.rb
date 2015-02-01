45e3401dda0a2f8b87ba00b6c1d52f0eeee27bab
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-01 14:04:14 +0200

    User ratings and memberships are dependent on user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
-	has_many :ratings
+	has_many :ratings, dependent: :destroy
 	has_many :beers, through: :ratings
 	
-	has_many :memberships
+	has_many :memberships, dependent: :destroy
 	has_many :beer_clubs, through: :memberships
 
 	validates :username, uniqueness: true, length: {minimum: 3, maximum: 15}
