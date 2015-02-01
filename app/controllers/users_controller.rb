6e9b6bb3a1ca02ae7c85640478904ba140eef521
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-01 14:13:42 +0200

    Username added to passwor editing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,16 +40,14 @@ class UsersController < ApplicationController
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
-  def update
-    if (current_user.id == @user.id)
-      respond_to do |format|
-        if @user.update(user_params)
-          format.html { redirect_to @user, notice: 'User was successfully updated.' }
-          format.json { render :show, status: :ok, location: @user }
-        else
-          format.html { render :edit }
-          format.json { render json: @user.errors, status: :unprocessable_entity }
-        end
+   def update
+    respond_to do |format|
+      if user_params[:username].nil? and @user == current_user and @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
