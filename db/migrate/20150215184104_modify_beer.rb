83e0a6143d92ce7aab01054db4e753ecf96b20fc
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-15 23:19:04 +0200

    Modified migration to cast string to integer properly

--- db/migrate/20150215184104_modify_beer.rb
+++ db/migrate/20150215184104_modify_beer.rb
@@ -1,5 +1,5 @@
 class ModifyBeer < ActiveRecord::Migration
   def change
-  	change_column :beers, :style, :integer
+  	change_column :beers, :style, :integer,  'integer USING CAST(column_name AS integer)'
   end
 end
