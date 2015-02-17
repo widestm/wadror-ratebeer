a39cdd07f93c45e20ba90f5ab35e22a3204983a0
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-17 17:19:28 +0200

    Refactored tests to handle brewery activity

--- spec/features/breweries_page_spec.rb
+++ spec/features/breweries_page_spec.rb
@@ -4,33 +4,56 @@ describe "Breweries page" do
   it "should not have any before been created" do
     visit breweries_path
     expect(page).to have_content 'Listing breweries'
-    expect(page).to have_content 'Number of breweries: 0'
+    expect(page).to have_content 'Number of retired breweries: 0'
+    expect(page).to have_content 'Number of active breweries: 0'
+
 
   end
 
   describe "when breweries exists" do
     before :each do
-      @breweries = ["Koff", "Karjala", "Schlenkerla"]
+      @active_breweries = ["Koff", "Karjala", "Schlenkerla"]
+      @retired_breweries = ["Suomenlinnan_panimo", "Brew_dog"]
       year = 1896
-      @breweries.each do |brewery_name|
-        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
+
+      @active_breweries.each do |brewery_name|
+        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1, active: true)
+      end
+
+      @retired_breweries.each do |brewery_name|
+        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1, active: false)
       end
 
       visit breweries_path
     end
 
-    it "lists the breweries and their total number" do
-      expect(page).to have_content "Number of breweries: #{@breweries.count}"
-      @breweries.each do |brewery_name|
+    it "lists all active breweries and their total count" do
+      expect(page).to have_content "Number of active breweries: #{@active_breweries.count}"
+      @active_breweries.each do |brewery_name|
         expect(page).to have_content brewery_name
       end
     end
 
-    it "allows user to navigate to page of a Brewery" do
+    it "lists all retired breweries and their total count" do
+      expect(page).to have_content "Number of retired breweries: #{@retired_breweries.count}"
+      @retired_breweries.each do |brewery_name|
+        expect(page).to have_content brewery_name
+      end
+    end
+
+    it "allows user to navigate to page of an active Brewery" do
       click_link "Koff"
 
       expect(page).to have_content "Koff"
       expect(page).to have_content "Established in 1897"
+    end
+
+    it "allows user to navigate to page of a retired Brewery" do
+      click_link "Suomenlinnan_panimo"
+
+      expect(page).to have_content "Suomenlinnan_panimo"
+      expect(page).to have_content "Established in 1900"
+      expect(page).to have_content "retired"
     end
 
   end
