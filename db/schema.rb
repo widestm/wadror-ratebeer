cc1f05b5fd690fb339a6a8352f93411311e828fb
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-17 16:20:30 +0200

    Added active field to brewery

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215190906) do
+ActiveRecord::Schema.define(version: 20150217140340) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20150215190906) do
     t.integer  "year"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
   create_table "memberships", force: :cascade do |t|
