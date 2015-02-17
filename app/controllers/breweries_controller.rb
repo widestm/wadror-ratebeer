ffa4b861ae0e5a020cb4d23e35f78b1d9c9bf8e0
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-17 16:22:28 +0200

    Implemented usage of active-field for brewery

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -4,7 +4,8 @@ class BreweriesController < ApplicationController
   # GET /breweries
   # GET /breweries.json
   def index
-    @breweries = Brewery.all
+    @active_breweries = Brewery.active
+    @retired_breweries = Brewery.retired 
   end
 
   # GET /breweries/1
@@ -69,7 +70,7 @@ class BreweriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def brewery_params
-      params.require(:brewery).permit(:name, :year)
+      params.require(:brewery).permit(:name, :year, :active)
     end
 
     private
