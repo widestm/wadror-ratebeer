eda96533414af4f2e806d7aec793ce7464533b8a
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-20 14:15:55 +0200

    Added helper method for edit and delete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def edit_and_destroy_buttons(item)
+		unless current_user.nil?
+			edit = link_to('Edit', url_for([:edit, item]), class:"btn btn-primary")
+			del = link_to('Destroy', item, method: :delete,
+				data: {confirm: "Are you sure you want to delete #{item.name}?"}, class:"btn btn-danger")
+			raw("#{edit} #{del}")
+		end
+	end
 end
