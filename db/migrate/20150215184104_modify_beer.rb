0e104cbab1fa5016d7f77ff85253941c5819d71a
Mikael Wide <mikael.wide@helsinki.fi>
2015-02-20 19:20:24 +0200

    Added comment [ci skip]

--- db/migrate/20150215184104_modify_beer.rb
+++ db/migrate/20150215184104_modify_beer.rb
@@ -3,3 +3,5 @@ class ModifyBeer < ActiveRecord::Migration
   	change_column :beers, :style, :integer
   end
 end
+# when rebuilding psql db use following:
+# change_column :beers, :style, 'integer USING CAST(style AS integer)'
