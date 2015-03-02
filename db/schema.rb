192413bf83d0cefc39ed418d5fac0648b55916be
Mikael Wide <mikael.wide@helsinki.fi>
2015-03-02 13:27:22 +0200

    Add index to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227121759) do
+ActiveRecord::Schema.define(version: 20150302112637) do
 
   create_table "beer_clubs", force: :cascade do |t|
     t.string   "name"
